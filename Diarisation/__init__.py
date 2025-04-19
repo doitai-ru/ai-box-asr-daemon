5ce570b5e3ec542198c6bd2dd1c3df1f29531803
Sanich137 <Sanich137@gmail.com>
2025-04-19 09:07:23 +0300

    Реализовано выключение диаризации и скачивание модели по названию.

--- Diarisation/__init__.py
+++ Diarisation/__init__.py
@@ -2,12 +2,13 @@ import config
 from utils.pre_start_init import paths
 from utils.do_logging import logger
 from VoiceActivityDetector import vad
+from collections import defaultdict
 import requests
 
 
 if config.CAN_DIAR:
     if not paths.get("diar_speaker_model_path").exists():
-        logger.error(f"Модель для диаризации не найдена. Предпринимаются попытки скачать {config.DIAR_MODEL_NAME}.onnx")
+        logger.error(f"Модель для диаризации не найдена. Предпринимаются попытки скачать {config.DIAR_MODEL_NAME}")
         output_path = paths.get("diar_speaker_model_path")
         api_url = "https://modelscope.cn/api/v1/datasets/wenet/wespeaker_pretrained_models/oss/tree"
 
@@ -16,10 +17,21 @@ if config.CAN_DIAR:
             response = requests.get(api_url, headers={"User-Agent": "Mozilla/5.0"})
             response.raise_for_status()
             # 2. Ищем нужный файл
-            target_file = "voxblink2_samresnet100_ft.onnx"
+            target_file = config.DIAR_MODEL_NAME
             file_data = next((item for item in response.json()["Data"] if item["Key"] == target_file), None)
 
             if not file_data:
+                logger.error(f"Модели с именем {config.DIAR_MODEL_NAME} в списке возможных для загрузки нет.")
+                # Получаем список всех ONNX-моделей
+                onnx_models_with_size = [
+                    (item['Key'], item['Size'] // (1024 * 1024))
+                    for item in response.json()["Data"]
+                    if item['Key'].endswith('.onnx')
+                ]
+                logger.info("Доступны для скачивания следующие модели:")
+                for name, size in sorted(onnx_models_with_size):
+                    logger.info(f"{name} - {size} MB")
+
                 raise Exception("Файл не найден в API") # Todo - указать тут список доступных моделей из response.json()["Data"]
 
             # 3. Скачиваем по прямой ссылке
--- config.py
+++ config.py
@@ -31,13 +31,18 @@ PUNCTUATE_WITH_GPU = os.getenv('VAD_WITH_GPU', True)   # Если потребу
 
 # Diarisation_settings
 CAN_DIAR = True if int(os.getenv('CAN_DIAR', 0)) == 1 else False
-DIAR_MODEL_NAME = os.getenv('DIAR_MODEL_NAME', "voxblink2_samresnet100_ft")
+DIAR_MODEL_NAME = str(os.getenv('DIAR_MODEL_NAME', "voxblink2_samresnet100_ft")+".onnx")
 DIAR_WITH_GPU = os.getenv('DIAR_WITH_GPU', False)
 CPU_WORKERS = int(os.getenv('CPU_WORKERS', 0)) # Для значений меньше 1 будут использованы все доступные ядра -1. При больше 1  - указанное число ядер CPU. Работает только при GPU True
 
+# Разных моделей для диаризации много.
+# Если voxblink2_samresnet100_ft работает на вашей мощности не достаточно быстро, выберите модель меньшего размера:
+# [('cnceleb_resnet34.onnx', 25), ('cnceleb_resnet34_LM.onnx', 25), ('voxblink2_samresnet100.onnx', 191),
+# ('voxblink2_samresnet100_ft.onnx', 191), ('voxblink2_samresnet34.onnx', 96), ('voxblink2_samresnet34_ft.onnx', 96),
+# ('voxceleb_CAM++.onnx', 27), ('voxceleb_CAM++_LM.onnx', 27), ('voxceleb_ECAPA1024.onnx', 56),
+# ('voxceleb_ECAPA1024_LM.onnx', 56), ('voxceleb_ECAPA512.onnx', 23), ('voxceleb_ECAPA512_LM.onnx', 23),
+# ('voxceleb_gemini_dfresnet114_LM.onnx', 24), ('voxceleb_resnet152_LM.onnx', 75), ('voxceleb_resnet221_LM.onnx', 90),
+# ('voxceleb_resnet293_LM.onnx', 109), ('voxceleb_resnet34.onnx', 25), ('voxceleb_resnet34_LM.onnx', 25)]
 
 
 print(f"Using '{LOGGING_LEVEL}' LOGGING_LEVEL")
-
-
-
