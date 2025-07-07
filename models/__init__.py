2a018844fb9e9111a5eb1274f20b3b9ad0c85a26
Sanich137 <Sanich137@gmail.com>
2025-07-07 13:28:58 +0300

    Правки косметические.

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,5 @@
 from . import fast_api_models
+from .fast_api_models import WebSocketModel
+from .fast_api_models import SyncASRRequest
+from .fast_api_models import PostFileRequest
+from .fast_api_models import PostFileRequestDiarize
